9456bd5c225856a384968ba7f587f75ced67a77d
lin <123>
2020-08-22 14:15:21 +0800

    调用service层，查询商品表

--- web/src/main/java/com/jd/servlet/CommodityServlet.java
+++ web/src/main/java/com/jd/servlet/CommodityServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -22,15 +23,18 @@ public class CommodityServlet extends HttpServlet {
         doRequest(req,resp);
     }
 
-    private void doRequest(HttpServletRequest req, HttpServletResponse resp) {
+    private void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String servletPath = req.getServletPath();
         if ("/commodity".equals(servletPath)){
             commodity(req,resp);
         }
     }
 
-    private void commodity(HttpServletRequest req, HttpServletResponse resp) {
+    private void commodity(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //调用service层，查询商品表
         List<Products> products = commodityService.selectAll();
+        HttpSession session = req.getSession();
+        session.setAttribute("products",products);
+        req.getRequestDispatcher("/WEB-INF/shooping.jsp").forward(req,resp);
     }
 }
